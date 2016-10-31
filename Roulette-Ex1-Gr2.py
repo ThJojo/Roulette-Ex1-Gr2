31ea4d77e31e67b04a47c7f567d6aaf3d63ca8d7
Harald <die.elektro.post@posteo.org>
2016-11-01 00:29:15 +0100

    asking for budget
    request input of a valid budget (positive numeral value)

--- Roulette-Ex1-Gr2.py
+++ Roulette-Ex1-Gr2.py
@@ -10,4 +10,22 @@ os.system('cls' if os.name == 'nt' else 'clear')
 # Asking for the player's name
 player = input('Welcome to Terminal Roulette!\n\n'
              + 'Please, enter your name: ')
-print('\n' + 'Hello', player + '!' , '\n')
+print('\n' + 'Hello', player + '!' + '\n') # May remove for final version
+
+# Asking the player for the budget
+# https://wiki.python.org/moin/WhileLoop
+# http://stackoverflow.com/a/4138231
+# http://stackoverflow.com/a/4730961
+while True:
+    budget = input('How big is your budget for this game? ')
+    try:
+        budget = float(budget)
+        if budget > 0:
+            break
+        else:
+            print('\n' + 'Your input must be greater than zero!')
+    except ValueError:
+        print('\n' +'Only Arabic numerals are accepted as input!')
+        False
+
+print('\n' + 'Your budget is {:.2f}'.format(budget) + '\n') # May remove for final version
