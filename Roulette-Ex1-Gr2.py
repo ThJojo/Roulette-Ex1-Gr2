b5977e574a71e92dd20c9ce4949ae2093931bdbb
Harald <die.elektro.post@posteo.org>
2016-10-31 21:42:12 +0100

    implementing Listing 1
    The code was provided in "Ex-1_simple_roulette.pdf"

--- Roulette-Ex1-Gr2.py
+++ Roulette-Ex1-Gr2.py
@@ -1,4 +1,8 @@
 # 120.050 Introduction to Python programming for geoscience
 # Exercise 1 - Group 2
 # 2016 October and November
-#
+
+# Listing 1 from "Ex-1_simple_roulette.pdf"
+# This snippet clears the screan of a terminal
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
