2adc8e6f9eb1a6375c98ebaebd1083cf3d7667c4
Harald <die.elektro.post@posteo.org>
2016-11-01 19:48:57 +0100

    implenting bet types
    all bet types were implemented. for most of them range(list(...)) was used. yet, a logical explanation for the numbers of rouge and noir is missing.

--- Roulette-Ex1-Gr2.py
+++ Roulette-Ex1-Gr2.py
@@ -30,3 +30,22 @@ while True:
 
 # Listing 2 from "Ex-1_simple_roulette.pdf" used for float format
 print('\n' + 'Your budget is {:.2f}'.format(budget) + '\n') # May remove for final version
+
+# Implementing the different bet types
+# http://stackoverflow.com/a/2184764
+single = list(range(37))     # creates a list with the numbers  0..36
+manque = list(range(1,19))   #                                  1..18
+passe = list(range(19,37))   #                                 19..36
+rouge = [ 1,  3,  5,  7,  9, 12,
+         14, 16, 18, 19, 21, 23,
+         25, 27, 30, 32, 34, 36] # list of "rouge" numbers
+                                 # logical explanation for distribution not yet found!
+noir = [ 2,  4,  6,  8, 10, 11,
+        13, 15, 17, 20, 22, 24,
+        26, 28, 29, 31, 33, 35] # list of "rouge" numbers
+                                # logical explanation for distribution not yet found!
+pair = list(range(2,37,2))   # even numbers between 1..36
+impair = list(range(1,37,2)) # odd numbers between  1..36
+dozen1 = list(range(1,13))   # creates a list with the numbers  1..12
+dozen2 = list(range(13,25))  #                                 13..24
+dozen3 = list(range(25,37))  #                                 25..36
